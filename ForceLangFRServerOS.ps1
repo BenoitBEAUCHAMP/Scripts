9d9535bc00c4e39a6f93aa766d2dff557532a9a4
BenoitBEAUCHAMP <benoit.beauchamp@wanadoo.fr>
2025-07-23 10:29:54 +0200

    Add files via upload

---
+++ ForceLangFRServerOS.ps1
@@ -0,0 +1,10 @@
+﻿$system = Get-WmiObject Win32_OperatingSystem | Select-Object OSArchitecture
+$mountResult = Mount-DiskImage -ImagePath "C:/temp/lang.iso" -StorageType ISO -PassThru
+$driveLetter = ($mountResult | Get-Volume).DriveLetter
+if ($system.OSArchitecture -eq "64 bits") {
+    Dism /online /Add-Package /PackagePath:${driveLetter}:/LanguagesAndOptionalFeatures/Microsoft-Windows-Server-Language-Pack_x64_fr-fr.cab
+}
+else {
+    Write-Output "OS Sytem non pris en charge : pas 64bits"
+}
+Dismount-DiskImage -ImagePath "C:/temp/lang.iso"
