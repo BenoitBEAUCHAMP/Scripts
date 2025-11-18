a1cc3c18cee969719c150d087aa720c0e7c551b0
BenoitBEAUCHAMP <benoit.beauchamp@wanadoo.fr>
2025-11-18 17:10:41 +0100

    Update LangFR-AVD.ps1

--- LangFR-AVD.ps1
+++ LangFR-AVD.ps1
@@ -16,6 +16,20 @@ else {
   $GeoId = "84"
   $TimeZone = "Romance Standard Time"
   $Locale = "0000040C"   # FR keyboard
+  $defaultIntl = "Registry::HKEY_USERS\.DEFAULT\Control Panel\International"
+  $defaultKeyboard = "Registry::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload"
+  # FR regional settings COMPLETS
+  $intlValues = @{
+    "LocaleName"    = "fr-FR"
+    "Locale"        = "0000040C"
+    "iCountry"      = "33"
+    "sCountry"      = "France"
+    "iCalendarType" = "1"
+    "sShortDate"    = "dd/MM/yyyy"
+    "sLongDate"     = "dddd d MMMM yyyy"
+    "sTimeFormat"   = "HH:mm:ss"
+    "iFirstDayOfWeek" = "1"
+  }
   
   #LanguagePack FR
   Install-Language $Language
@@ -44,37 +58,20 @@ if ($Mode -eq "Modern") {
   $LangList[0].InputMethodTips.Clear()
   $LangList[0].InputMethodTips.Add("040C:$Locale")
   Set-WinUserLanguageList $LangList -Force
-  # Locale
-  Set-ItemProperty "Registry::HKEY_USERS\.DEFAULT\Control Panel\International" -Name "LocaleName" -Value "fr-FR"
-  Set-ItemProperty "Registry::HKEY_USERS\.DEFAULT\Control Panel\International" -Name "Locale" -Value "0000040C"
-  # Formats
-  Set-ItemProperty "Registry::HKEY_USERS\.DEFAULT\Control Panel\International" -Name "sShortDate" -Value "dd/MM/yyyy"
-  Set-ItemProperty "Registry::HKEY_USERS\.DEFAULT\Control Panel\International" -Name "sLongDate"  -Value "dddd d MMMM yyyy"
-  Set-ItemProperty "Registry::HKEY_USERS\.DEFAULT\Control Panel\International" -Name "sTimeFormat" -Value "HH:mm:ss"
-  Set-ItemProperty "Registry::HKEY_USERS\.DEFAULT\Control Panel\International" -Name "iFirstDayOfWeek" -Value "1"
-  # Keyboard
-  New-Item "Registry::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Force | Out-Null
-  Set-ItemProperty "Registry::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "1" -Value "0000040C"
-  # Input method patches (Win11 24H2+ requirement)
-  New-Item "Registry::HKEY_USERS\.DEFAULT\Keyboard Layout\Substitutes" -Force | Out-Null
-  Set-ItemProperty "Registry::HKEY_USERS\.DEFAULT\Keyboard Layout\Substitutes" -Name "00000409" -Value "0000040C"
-  # UI Language
-  New-Item "Registry::HKEY_USERS\.DEFAULT\Control Panel\Desktop" -Force | Out-Null
-  Set-ItemProperty "Registry::HKEY_USERS\.DEFAULT\Control Panel\Desktop" -Name "PreferredUILanguages" -Value "fr-FR"
-  Set-ItemProperty "Registry::HKEY_USERS\.DEFAULT\Control Panel\Desktop" -Name "PreferredUILanguagesPending" -Value "fr-FR"
-  ## SYSTEM profile
-  # Locale
-  Set-ItemProperty "Registry::HKEY_USERS\S-1-5-18\Control Panel\International" -Name "LocaleName" -Value "fr-FR"
-  Set-ItemProperty "Registry::HKEY_USERS\S-1-5-18\Control Panel\International" -Name "Locale" -Value "0000040C"
-  # Keyboard
-  New-Item "Registry::HKEY_USERS\S-1-5-18\Keyboard Layout\Preload" -Force | Out-Null
-  Set-ItemProperty "Registry::HKEY_USERS\S-1-5-18\Keyboard Layout\Preload" -Name "1" -Value "0000040C"
-  # Input method consistency (important for AVD)
-  New-Item "Registry::HKEY_USERS\S-1-5-18\Keyboard Layout\Substitutes" -Force | Out-Null
-  Set-ItemProperty "Registry::HKEY_USERS\S-1-5-18\Keyboard Layout\Substitutes" -Name "00000409" -Value "0000040C"
-  # UI language
-  New-Item "Registry::HKEY_USERS\S-1-5-18\Control Panel\Desktop" -Force | Out-Null
-  Set-ItemProperty "Registry::HKEY_USERS\S-1-5-18\Control Panel\Desktop" -Name "PreferredUILanguages" -Value "fr-FR"
+  foreach ($k in $intlValues.Keys) {
+    Set-ItemProperty -Path $defaultIntl -Name $k -Value $intlValues[$k]
+  }
+  # Clavier
+  New-Item -Path $defaultKeyboard -Force | Out-Null
+  Set-ItemProperty -Path $defaultKeyboard -Name "1" -Value $Locale
+  #COMPTE SYSTEM et new users
+  $systemIntl = "Registry::HKEY_USERS\S-1-5-18\Control Panel\International"
+  $systemKeyboard = "Registry::HKEY_USERS\S-1-5-18\Keyboard Layout\Preload"
+  foreach ($k in $intlValues.Keys) {
+    Set-ItemProperty -Path $systemIntl -Name $k -Value $intlValues[$k]
+  }
+  New-Item -Path $systemKeyboard -Force | Out-Null
+  Set-ItemProperty -Path $systemKeyboard -Name "1" -Value $Locale
 }
   # restart virtual machine to apply regional settings to current user. 
   Start-sleep -Seconds 40
