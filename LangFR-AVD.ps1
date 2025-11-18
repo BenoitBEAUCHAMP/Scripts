137b09b5519c385d93bb5a2b99d5aeb372ee9cb1
BenoitBEAUCHAMP <benoit.beauchamp@wanadoo.fr>
2025-11-18 12:16:54 +0100

    Update LangFR-AVD.ps1
    Update script Win11 <= 23H2 et Win11 > 23H2

--- LangFR-AVD.ps1
+++ LangFR-AVD.ps1
@@ -1,31 +1,78 @@
-#variables
-$regionalsettingsURL = "https://raw.githubusercontent.com/BenoitBEAUCHAMP/Scripts/refs/heads/main/FRRegion.xml"
-$RegionalSettings = "C:\Region.xml"
-$Language = "fr-FR"
-$GeoId = "84"
-$TimeZone = "Romance Standard Time"
+$Build = [System.Environment]::OSVersion.Version.Build
+if ($Build -lt 26100) {
+    Write-Host "Windows 11 <= 23H2 detected. Running Legacy configuration." -ForegroundColor Green
+    $Mode = "Legacy"
+}
+else {
+    Write-Host "Windows 11 24H2+ detected. Running Modern configuration." -ForegroundColor Yellow
+    $Mode = "Modern"
+}
 
-#LanguagePack Suisse
-Install-Language $Language
+if ($Mode -eq "Legacy") {
+  #variables
+  $regionalsettingsURL = "https://raw.githubusercontent.com/BenoitBEAUCHAMP/Scripts/refs/heads/main/FRRegion.xml"
+  $RegionalSettings = "C:\Region.xml"
+  $Language = "fr-FR"
+  $GeoId = "84"
+  $TimeZone = "Romance Standard Time"
 
-#downdload regional settings file
-$webclient = New-Object System.Net.WebClient
-$webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
+  #LanguagePack Suisse
+  Install-Language $Language
 
-#LanguagePack USA
-unInstall-Language "en-US"
-Start-sleep -Seconds 120
+  #downdload regional settings file
+  $webclient = New-Object System.Net.WebClient
+  $webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
 
-# Set languages/culture. Not needed perse.
-Set-WinSystemLocale $Language
-Set-WinUserLanguageList -LanguageList $Language -Force
-Set-Culture -CultureInfo $Language
-Set-WinHomeLocation -GeoId $GeoId 
-Set-TimeZone -id $TimeZone
+  #LanguagePack USA
+  unInstall-Language "en-US"
+  Start-sleep -Seconds 120
 
-# Set Locale, language etc. 
-& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
+  # Set languages/culture. Not needed perse.
+  Set-WinSystemLocale $Language
+  Set-WinUserLanguageList -LanguageList $Language -Force
+  Set-Culture -CultureInfo $Language
+  Set-WinHomeLocation -GeoId $GeoId 
+  Set-TimeZone -id $TimeZone
 
-# restart virtual machine to apply regional settings to current user. 
-Start-sleep -Seconds 40
-Restart-Computer
+  # Set Locale, language etc. 
+  & $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
+
+  # restart virtual machine to apply regional settings to current user. 
+  Start-sleep -Seconds 40
+  Restart-Computer
+}
+if ($Mode -eq "Modern") {
+
+    $Language = "fr-FR"
+    $GeoId = 84
+    $TimeZone = "Romance Standard Time"
+
+    # New mandatory parameter for 24H2+
+    Install-Language -Language $Language -CopyToSystem -Force
+
+    # Remove US language
+    Uninstall-Language "en-US"
+    Start-Sleep -Seconds 60
+
+    # New recommended Microsoft method (24H2+)
+    Set-WinSystemLocale -SystemLocale $Language
+    Set-WinHomeLocation -GeoId $GeoId
+    Set-TimeZone -Id $TimeZone
+
+    # Apply full language pack to shell + login UI
+    Dism /Online /Set-Lang:$Language
+    Dism /Online /Set-SKUIntlDefaults:$Language
+
+    # Override UI language properly
+    Set-WinUILanguageOverride -Language $Language
+
+    # User language list
+    $LangList = New-WinUserLanguageList $Language
+    Set-WinUserLanguageList $LangList -Force
+
+    # Culture
+    Set-Culture $Language
+
+    # Reboot required twice in 24H2+
+    Restart-Computer -Force
+}
