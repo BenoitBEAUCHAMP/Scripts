ebb44f16b5e5bebe78d343c4617cf9bab8d8c749
BenoitBEAUCHAMP <benoit.beauchamp@wanadoo.fr>
2025-11-18 15:24:43 +0100

    Update LangFR-AVD.ps1

--- LangFR-AVD.ps1
+++ LangFR-AVD.ps1
@@ -1,11 +1,23 @@
+$Build = [System.Environment]::OSVersion.Version.Build
+$Mode = "Legacy"
+if ($Build -lt 26100) {
+    Write-Host "Windows 11 <= 23H2 detected. Running Legacy configuration." -ForegroundColor Green
+    $Mode = "Legacy"
+}
+else {
+    Write-Host "Windows 11 24H2+ detected. Running Modern configuration." -ForegroundColor Yellow
+    $Mode = "Modern"
+}
+
   #variables
   $regionalsettingsURL = "https://raw.githubusercontent.com/BenoitBEAUCHAMP/Scripts/refs/heads/main/FRRegion.xml"
   $RegionalSettings = "C:\Region.xml"
   $Language = "fr-FR"
   $GeoId = "84"
   $TimeZone = "Romance Standard Time"
-
-  #LanguagePack Suisse
+  $Locale = "0000040C"   # FR keyboard
+  
+  #LanguagePack FR
   Install-Language $Language
 
   #downdload regional settings file
@@ -23,9 +35,22 @@
   Set-WinHomeLocation -GeoId $GeoId 
   Set-TimeZone -id $TimeZone
 
+if ($Mode -eq "Legacy") {
   # Set Locale, language etc. 
   & $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
-
+}
+if ($Mode -eq "Modern") {
+  $LangList = New-WinUserLanguageList $Language
+  $LangList[0].InputMethodTips.Clear()
+  $LangList[0].InputMethodTips.Add("040C:$Locale")
+  Set-WinUserLanguageList $LangList -Force
+  Set-ItemProperty -Path "Registry::HKEY_USERS\.DEFAULT\Control Panel\International" -Name "LocaleName" -Value $Language
+  New-Item -Path "Registry::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Force | Out-Null
+  Set-ItemProperty -Path "Registry::HKEY_USERS\.DEFAULT\Keyboard Layout\Preload" -Name "1" -Value $Locale
+  Set-ItemProperty -Path "Registry::HKEY_USERS\S-1-5-18\Control Panel\International" -Name "LocaleName" -Value $Language
+  New-Item -Path "Registry::HKEY_USERS\S-1-5-18\Keyboard Layout\Preload" -Force | Out-Null
+  Set-ItemProperty -Path "Registry::HKEY_USERS\S-1-5-18\Keyboard Layout\Preload" -Name "1" -Value $Locale
+}
   # restart virtual machine to apply regional settings to current user. 
   Start-sleep -Seconds 40
   Restart-Computer
